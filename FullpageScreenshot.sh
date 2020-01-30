4cbef636fa41fd88fb0a0172b8a8fdfb0bea79b7
jivan <jramjisingh@gmail.com>
2020-01-30 11:47:49 -0500

    Added functionality for taking links

--- FullpageScreenshot.sh
+++ FullpageScreenshot.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 xdotool key  --clearmodifiers --delay 40 "super+1" 
+xdotool key --clearmodifiers --delay 40 "ctrl+t"
+xdotool type --clearmodifiers --delay 40 "$1"
+xdotool key --clearmodifiers --delay 40 "Return"
 xdotool key  --clearmodifiers --delay 40 "ctrl+shift+k" 
-sleep 2
-xdotool type  --clearmodifiers --delay 40 ":screenshot --fullpage" 
+sleep 5
+xdotool type  --clearmodifiers --delay 40 ":screenshot screenshot --fullpage" 
 xdotool key --clearmodifiers --delay 40 "Return"
