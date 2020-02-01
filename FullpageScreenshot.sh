f9b15e24e9370714cf9880b34f5eefdefda52e2c
jivan <jramjisingh@gmail.com>
2020-02-01 18:10:37 -0500

    Fixed extreme tab utilization

--- FullpageScreenshot.sh
+++ FullpageScreenshot.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 xdotool key  --clearmodifiers --delay 40 "super+1" 
-xdotool key --clearmodifiers --delay 40 "ctrl+t"
+
+# Uncomment this line and comment the other to change the script to continuously open new tabs instead of using the same one
+#xdotool key --clearmodifiers --delay 40 "ctrl+t"
+xdotool key --clearmodifiers --delay 40 "F6"
+
 xdotool type --clearmodifiers --delay 40 "$1"
 xdotool key --clearmodifiers --delay 40 "Return"
 xdotool key  --clearmodifiers --delay 40 "ctrl+shift+k" 
